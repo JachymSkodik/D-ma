27279fee5d3b36e0add9b6edc8b0e4e1160592e2
TomPribil <100162450+TomPribil@users.noreply.github.com>
2022-06-03 11:19:04 +0200

    přidány vlastnoti herních políček

---
+++ boardsquare.py
@@ -0,0 +1,34 @@
+import tkinter
+from enums import Color
+
+class BoardSquare:
+    def __init__ (self,color,positionX,positionY):
+        self.fig= None
+        self.color = color
+        self.positionX = positionX
+        self.positionY = positionY
+     
+    def hasFig(self): #ověření prázdnosti políčka
+        return self.fig != None
+    
+    def saveFig(self,newFig):
+        if(self.hasFig() and newFig != None):  
+            raise Exception("Can't save figure to square which contains figure: ", self.fig, newFig)
+        else:
+            self.fig = newFig #když je políčko prázdné, figurka se uloží
+        
+    def draw(self, rectangleWidth, rectangleHeight, canvas):
+    
+        rectangleFill = 'lightgrey' #střídání barev
+        if(self.color == Color.BLACK):
+            rectangleFill = 'brown'
+    
+        canvas.create_rectangle( #vytvoření čtverce pro políčka, jako parametry souřadnice rohů
+            rectangleWidth*self.positionX,
+            rectangleHeight*self.positionY,
+            rectangleWidth*(self.positionX+1),
+            rectangleHeight*(self.positionY+1),
+            fill=rectangleFill) #vyplnění barvou
+        
+        if(self.hasFig()):
+            self.fig.draw(rectangleWidth, rectangleHeight, canvas)
