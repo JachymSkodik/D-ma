01ab4e8f94960867e551a25be89935f06af62209
JachymSkodik <99656164+JachymSkodik@users.noreply.github.com>
2022-06-16 13:40:58 +0200

    úprava syntaxe

--- boardsquare.py
+++ boardsquare.py
@@ -2,33 +2,34 @@ import tkinter
 from enums import Color
 
 class BoardSquare:
-    def __init__ (self,color,positionX,positionY):
-        self.fig= None
+    def __init__ (self, color, position_x, position_y):
+        self.fig = None
         self.color = color
-        self.positionX = positionX
-        self.positionY = positionY
+        self.position_x = position_x
+        self.position_y = position_y
      
-    def hasFig(self): #ověření prázdnosti políčka
+    def has_fig(self): #ověření prázdnosti políčka
         return self.fig != None
     
-    def saveFig(self,newFig):
-        if(self.hasFig() and newFig != None):  
-            raise Exception("Can't save figure to square which contains figure: ", self.fig, newFig)
+    def save_fig(self, new_fig):
+        if(self.has_fig() and new_fig != None):  
+            raise Exception("Can't save figure to square which contains figure: ", self.fig, new_fig)
         else:
-            self.fig = newFig #když je políčko prázdné, figurka se uloží
+            self.fig = new_fig #když je políčko prázdné, figurka se uloží
         
-    def draw(self, rectangleWidth, rectangleHeight, canvas):
+    def draw(self, rectangle_width, rectangle_height, canvas):
     
-        rectangleFill = 'lightgrey'  #střídání barev
+        rectangle_fill = 'pink'  #střídání barev
         if(self.color == Color.BLACK):
-            rectangleFill = 'brown'
+            rectangle_fill = 'brown'
     
-        canvas.create_rectangle( #vytvoření čtverce pro políčka, jako parametry souřadnice rohů
-            rectangleWidth*self.positionX,
-            rectangleHeight*self.positionY,
-            rectangleWidth*(self.positionX+1),
-            rectangleHeight*(self.positionY+1),
-            fill=rectangleFill) #vyplnění barvou
+        #vytvoření čtverce pro políčka, jako parametry souřadnice rohů
+        canvas.create_rectangle(
+            rectangle_width*self.position_x,
+            rectangle_height*self.position_y,
+            rectangle_width*(self.position_x+1),
+            rectangle_height*(self.position_y+1),
+            fill = rectangle_fill) #vyplnění barvou
         
-        if(self.hasFig()):
-            self.fig.draw(rectangleWidth, rectangleHeight, canvas)
+        if(self.has_fig()):
+            self.fig.draw(rectangle_width, rectangle_height, canvas)
