c1152f20f8a8d179eee6dfe04fb68be93ff22acd
JachymSkodik <99656164+JachymSkodik@users.noreply.github.com>
2022-06-16 14:11:19 +0200

    úprava syntaxe

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from os.path import exists #ověřuje existenci souboru
 from gameLoader import GameLoader
 
 
-def loadBoardFromCsv(game):
-	currentDirectory = getcwd() #zjistí aktuální adresář a uloží do proměnné
+def load_board_from_csv(game):
+	current_directory = getcwd() #zjistí aktuální adresář a uloží do proměnné
 	#vyvolá okno pro výběr souboru a uloží do proměnné jméno souboru
-	filename = filedialog.askopenfilename(initialdir = currentDirectory, #otevře se v aktuální složce
+	filename = filedialog.askopenfilename(initialdir = current_directory, #otevře se v aktuální složce
                                           title = "Select a File", #hláška
                                           filetypes = (("Game csv saved files", #popisek při výběru ve file exploreru
                                                         "*.csv*"), #podporované soubory
@@ -19,13 +19,13 @@ def loadBoardFromCsv(game):
 
 	if len(filename) == 0 : return #když se nezvolí soubor, vyskočí z funkce (funguje zde jako break)
 
-	savedFile = open(filename) #otevře soubor se zvoleným jménem
-	gameBoardState, lastPlayer = GameLoader.playGame(savedFile) #je rozsekáno v gameLoader
+	saved_file = open(filename) #otevře soubor se zvoleným jménem
+	game_board_state, last_player = GameLoader.play_game(saved_file) #je rozsekáno v gameLoader
 	# Poté co je vše přečtené, můžeme zavřít stream
-	savedFile.close()
+	saved_file.close()
 
-	game.board = gameBoardState #vytvoří board dle csv
-	game.active_player = lastPlayer #nastaví posledního hráče z csv
+	game.board = game_board_state #vytvoří board dle csv
+	game.active_player = last_player #nastaví posledního hráče z csv
 
 	# načtením získáme posledního hráče co hrál, ale na tahu má být ten druhý
 	game.toggle_players()
@@ -39,11 +39,11 @@ def loadBoardFromCsv(game):
 file_writer = None
 #maximálně 10 csv souborů, každý dostane index
 for i in range (0,10):
-	fileName = f"dama{i}.csv"
-	if exists(fileName): #když existuje soubor se stejným jménem, přejde na další index
+	file_name = f"dama{i}.csv"
+	if exists(file_name): #když existuje soubor se stejným jménem, přejde na další index
 		continue
 	else:
-		file_writer = open(fileName, "w") #připraví se nový soubor pro zápis
+		file_writer = open(file_name, "w") #připraví se nový soubor pro zápis
 		break
 
 
@@ -55,12 +55,12 @@ root.title('Dáma™') #pojmenování okna
 game = Game(root, file_writer = file_writer) #spustí hru znova, přiřadí file_writer se zápisem
 
 # vytvoří tlačítko k načítání hry z csv
-# při stisku tlačítka spustí loadBoardFromCsv
-w = tkinter.Button(root, text = "Load game from csv file", command=lambda: loadBoardFromCsv(game))
+# při stisku tlačítka spustí load_board_from_csv
+w = tkinter.Button(root, text = "Load game from csv file", command=lambda: load_board_from_csv(game))
 # pack umístí tlačítko vedle hracího pole
 # parametr side určuje stranu, kam se tlačítko umístí
 # padx určí vzdálenost od pole a kraje okna
-w.pack(side=tkinter.RIGHT, padx=10)
+w.pack(side = tkinter.RIGHT, padx = 10)
 
 game.play() # spuštění hry
 
